bb33e0d428dc5dbe9f1b583127f5f77907df1f25
05lkbwx <05lkbwx@gmail.com>
2017-06-05 21:54:33 -0500

    Update 003.py

--- 003.py
+++ 003.py
@@ -1,3 +1,28 @@
+# world's best solution:
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type strs: List[str]
+        :rtype: int
+        """
+        if not s or len(s) == 0:
+            return 0
+        
+        start = 0
+        record = 0
+        dic = {}
+        
+        for end in range(len(s)):
+            char = s[end]
+            if char in dic and dic[char] >= start:
+                start = dic[char] + 1
+            if end - start + 1 > record:
+                record = end - start + 1 
+            dic[char] = end
+        
+        return record
+
+# my solution:
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
